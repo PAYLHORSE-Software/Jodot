30d8278a3ef6ceb31dac806252e08b678f154341
paylanon <paylhorse@proton.me>
2024-06-22 11:06:23 +0400

    Meta cleanup, demo polished

---
+++ demo/godot/script/scroll.gd
@@ -0,0 +1,18 @@
+# Once we can have this code in the language instead, Jodot hits 1.0
+
+extends Sprite2D
+
+var scroll_speed_x = -0.03
+var offset_x = 0.0
+var offset_y = 0.0
+
+func _ready():
+	if material == null:
+		var mat = ShaderMaterial.new()
+		mat.shader = preload("res://shader/scroll.gdshader")
+		self.material = mat
+
+func _process(delta):
+	offset_x += delta * scroll_speed_x
+	offset_x = fmod(offset_x, 1)
+	material.set_shader_parameter("offset", Vector2(offset_x, offset_y))
